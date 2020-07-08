b1d025ff11c21544b7076e73cf517bd5d1b41f15
Ben Braithwaite <benjamin200@hotmail.co.uk>
2020-07-08 14:04:19 +0100

    marked up hangman code for assignment

--- hangman.py
+++ hangman.py
@@ -1,21 +1,29 @@
 import random
 import time
 
+# ==========Extract from file==========
+# Extract the words from word_list.txt and insert them into a list
+
 word_list = []
-playing = True
 
 with open('word_list.txt', 'r') as word_list_file:
     for line in word_list_file:
         line = line.strip()
         word_list.append(line)
 
+# ==========Game initialisation==========
+# Randomly select a word from word_list and initialise all game attributes
+
 secret_word = random.choice(word_list)
 
+playing = True
 guesses = 0
-secret_word_length = len(secret_word)
 all_guesses = []
 word_characters = list(secret_word)
-displayed_word = list('*'*secret_word_length)
+displayed_word = list('*'*len(secret_word))
+
+# ==========Input processing==========
+# Check the letter the user has picked to see if it is in the secret word
 
 def char_checker(guess):
     global guesses
@@ -28,7 +36,6 @@ def char_checker(guess):
         all_guesses.append(guess)
 
         if guess in word_characters:
-
             for i, char in enumerate(word_characters):
                 if char == guess:
                     displayed_word[i] = guess
@@ -37,17 +44,23 @@ def char_checker(guess):
             guesses += 1
             print(f"incorrect - wrong guesses: {guesses}/7")
 
+
+# ==========Game Loop==========
+# Prompt the user to choose a character and check it off against the secret word.
+# Terminates the script if user uses up their 7 guesses or if they get the word.
+# Uses the sleep function to be slightly enhance user experience
+
+
 print("Let's play Hangman!")
 time.sleep(2)
 print("Pick a letter, or type 'guesses' to see what you've already guessed")
 time.sleep(2)
 print("\n")
-# print("Your word to guess: ")
 
 while playing:
     time.sleep(1)
     print("Word: " + ''.join(displayed_word))
-    
+
     if ''.join(displayed_word) == secret_word:
         print("Congratulations you win")
         playing = False
