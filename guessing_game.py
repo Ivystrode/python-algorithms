c16f6e37d149686dd85aa01481ef5312ba061ff3
Ben Braithwaite <benjamin200@hotmail.co.uk>
2020-07-09 08:05:37 +0100

    refactor guessing game

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,6 @@
 import time
 import random
 
-number_known = False
 guesses = 0
 hpn = int(input("Pick a number to be the maximum value: "))
 lpn = 1
@@ -15,54 +14,63 @@ def countdown(seconds):
         print(str(seconds) + "...")
         seconds -= 1
 
+def picknumber():
+    global target
+    israndom = input("User pick a number or shall I?\n")
+
+    if 'user' in israndom or 'me' in israndom or 'i will' in israndom:
+        target = int(input("Enter a number between 1 and " + str(hpn) + "\n"))
+
+        if target > hpn or target < lpn:
+            print("you need to pick a number between " + str(lpn) + " and " + str(hpn))
+            picknumber()
+
+    else:
+        target = random.choice(possibles)
 
 def guess(possibles, lpn, hpn, *args):
-    global number_known
     global guesses
-    while number_known == False:
-        if args:
-            mid = (hpn + lpn) // 2
-            print("\nChecking " + str(mid))
-            time.sleep(0.5) # not necessary, just so you can watch it work through it
+    if args:
+        mid = (hpn + lpn) // 2
+        print("\nChecking " + str(mid))
+        time.sleep(0.25)
 
-            if mid < args[0]:
-                lpn = mid
-                print("Too low; guess higher")
-                time.sleep(0.5) # not necessary, just so you can watch it work through it
-                guesses += 1
-                guess(possibles, lpn, hpn, target)
-            elif mid > args[0]:
-                hpn = mid
-                print("Too high; guess lower")
-                time.sleep(0.5) # not necessary, just so you can watch it work through it
-                guesses += 1
-                guess(possibles, lpn, hpn, target)
-            else:
-                time.sleep(0.5)
-                print("Completed")
-                print("Secret number is: " + str(mid))
-                print("actual answer: " + str(target))
-                print("That took " + str(guesses+1) + " guesses")
-                number_known = True
-
-        else:
-            time.sleep(1)
-            mid = (hpn + lpn) // 2
-            answer = input("Is your number " + str(mid) + "\n")
-
+        if mid < args[0]:
+            lpn = mid
+            print("Too low; guess higher")
+            time.sleep(0.25)
             guesses += 1
+            guess(possibles, lpn, hpn, target)
+        elif mid > args[0]:
+            hpn = mid
+            print("Too high; guess lower")
+            time.sleep(0.25)
+            guesses += 1
+            guess(possibles, lpn, hpn, target)
+        else:
+            time.sleep(0.25)
+            print("Completed")
+            print("Secret number is: " + str(mid))
+            print("actual answer: " + str(target))
+            print("That took " + str(guesses+1) + " guesses")
 
-            if 'yes' in answer:
-                print("That took me " + str(guesses) + " guess/es to figure out")
-                number_known = True
-            elif 'higher' in answer:
-                lpn = mid
-                guess(possibles, lpn, hpn)
-            elif 'lower' in answer:
-                hpn = mid
-                guess(possibles, lpn, hpn)
-            else:
-                print("Unknown input")
+    else:
+        time.sleep(1)
+        mid = (hpn + lpn) // 2
+        answer = input("Is your number " + str(mid) + "\n")
+
+        guesses += 1
+
+        if 'yes' in answer:
+            print("That took me " + str(guesses) + " guess/es to figure out")
+        elif 'higher' in answer:
+            lpn = mid
+            guess(possibles, lpn, hpn)
+        elif 'lower' in answer:
+            hpn = mid
+            guess(possibles, lpn, hpn)
+        else:
+            print("Unknown input")
 
 if 'user' in gamechoice or 'me' in gamechoice:
     print("Think of a number between 1 and " + str(hpn))
@@ -70,15 +78,7 @@ if 'user' in gamechoice or 'me' in gamechoice:
     guess(possibles, lpn, hpn)
 
 else:
-    israndom = input("User pick a number or shall I?\n")
-
-    if 'user' in israndom or 'me' in israndom or 'i will' in israndom:
-        target = int(input("Enter a number between 1 and " + str(hpn) + "\n"))
-        if target > hpn or target < lpn:
-            print("you need to pick a number between " + str(lpn) + " and " + str(hpn))
-            exit()
-    else:
-        target = random.choice(possibles)
+    picknumber()
 
     print("Secret number: " + str(target))
     print("Script will now try to guess what the secret number is")
